16d8786ff68408b720a81262d70139a232e383cc
Sahatsawat Kanpai <keyboard2543@gmail.com>
2025-01-15 15:36:17 +0700

    ✨  able to store text/string value for each account

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -26,4 +26,57 @@ contract Storage {
     function retrieve() public view returns (uint256){
         return number;
     }
+
+    function checkETHBalance(address _account) public view returns (uint256) {
+        return address(_account).balance;
+    }
+
+    function giveEther(address payable _receiver) public payable {
+        _receiver.transfer(msg.value);
+    }
+
+    // // Mapping to store values for each address
+    // mapping(address => uint256) private values;
+
+    // // Event to log when a value is stored
+    // event ValueStored(address indexed user, uint256 value);
+
+    // // Function to store a value
+    // function storeValue(uint256 _value) public {
+    //     require(values[msg.sender] == 0, "Value already set for this account");
+
+    //     // Store the value in the mapping
+    //     values[msg.sender] = _value;
+
+    //     // Emit an event
+    //     emit ValueStored(msg.sender, _value);
+    // }
+
+    // // Function to retrieve the caller's stored value
+    // function getValue() public view returns (uint256) {
+    //     return values[msg.sender];
+    // }
+
+    // Mapping to store a string for each address
+    mapping(address => string) private values;
+
+    event StringStored(address indexed user, string value);
+
+    // Store a string associated with the sender's address
+    function storeValue(string calldata _value) public {
+        require(bytes(values[msg.sender]).length == 0, "Value already set for this account");
+        require(bytes(_value).length > 0, "String cannot be empty");
+
+        values[msg.sender] = _value;
+
+        // Emit an event
+        emit StringStored(msg.sender, _value);
+    }
+
+    // Retrieve the string associated with the sender's address
+    function getValue() public view returns (string memory) {
+        require(bytes(values[msg.sender]).length != 0, "No value set for this account");
+
+        return values[msg.sender];
+    }
 }
